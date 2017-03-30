e8c0126257bc26b94b9244c1075983ad5630a0e4
Łukasz Woźniak <ukasz Woniak>
2017-03-30 20:31:03 +0200

    delete double .concat, indentation correct

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,11 @@ var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 	maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 	allNames = femaleNames.concat(maleNames);
 	newName = 'Kacper';
-	allNames = femaleNames.concat(maleNames);
-if (allNames.indexOf(newName) === -1)
-{
-allNames.push(newName);
-document.write(allNames)
+if (allNames.indexOf(newName) === -1) {
+  allNames.push(newName);
+  document.write(allNames);
 } 
-else 
-{
-document.write('imię już występuje - wpisz inne')
+else {
+  document.write('imię już występuje - wpisz inne')
 }
 console.log(allNames)
-console.log(allNames+newName)
